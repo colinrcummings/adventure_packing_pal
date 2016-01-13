503a5484f913a6478ae46dabc68e8d2e920ac035
Colin Cummings <colinrcummings@gmail.com>
2016-01-13 13:51:59 -0500

    - add item styling - add conditionally styled backpack icon with ng-click directive tied to togglePacked() method

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Adventure Packing Pal (APP)
 An outdoorsy spin on the typical todo app, built with AngularJS v1.3.8
+
+## To do
+- Group items by Unpacked and Packed inside collapsable containers with count
+- Move lists to json data and load via XHR
+- Enable item adding, editing, deleting in state
+- Add different list types (mountaineering, backpacking, hiking, climbing)
+- Add required/optional
+- Integrate seasonality
--- js/controllers/PackingController.js
+++ js/controllers/PackingController.js
@@ -1,24 +1,32 @@
 app.controller('PackingController', ['$scope', function($scope) {
-  // TODO make an exteral JSON loaded by XHR and enable user to add / delete items
   $scope.items = [
     {
-      name: 'Backpack',
+      name: 'Pack',
+      description: '30L Worksack',
       packed: false
     },
     {
-      name: 'Jacket',
+      name: 'Midlayer',
+      description: 'R1 Pullover',
       packed: false
     },
     {
       name: 'Boots',
+      description: 'Vasques with 400 gram Thinsulate',
       packed: false
     },
     {
       name: 'Food',
+      description: 'Kind bars, cheese, shot blocks',
       packed: false
     }
   ];
-  $scope.setToPacked = function(index) {
-    $scope.items[index].packed = true;
+  $scope.togglePacked = function(index) {
+    var item = $scope.items[index];
+    if(item.packed === true) {
+      item.packed = false;
+    } else {
+      item.packed = true;
+    }
   };
 }]);
